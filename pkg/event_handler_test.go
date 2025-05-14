947bc1537bed789353f7bbf754a4c4038d57dadf
Otavio Valadares <otaviopvaladares@gmail.com>
2025-05-13 22:56:04 -0300

    Add missing tests

--- pkg/event_handler_test.go
+++ pkg/event_handler_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestEventHandlerHandle_HandleAcquireLockEvent(t *testing.T) {
-	lockEvent := &storage.Lock{
+	lockEvent := &domain.AcquireLockEvent{
 		Key:    "test-key",
 		NodeID: "test-node",
 	}
